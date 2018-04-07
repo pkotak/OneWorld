f5ae57299389cbe11af38e5ec0114b04aa8883be
jaydoshi95 <jay007199504@gmail.com>
2018-04-07 16:47:41 -0400

    Initial Admin commit

---
+++ src/main/java/edu/northeastern/oneworld/models/Admin.java
@@ -0,0 +1,17 @@
+package edu.northeastern.oneworld.models;
+
+import java.sql.Date;
+import java.util.List;
+
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import javax.persistence.OneToMany;
+
+import com.fasterxml.jackson.annotation.JsonIgnore;
+
+@Entity
+public class Admin extends Person{
+	
+}
