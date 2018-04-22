00f45d3fc6e253821cb6b7e9555343598ee86b60
jaydoshi95 <jay007199504@gmail.com>
2018-04-22 06:47:06 -0400

    Completed user and admin profile

---
+++ src/main/webapp/public/Profile/js/update-owner.js
@@ -0,0 +1,52 @@
+$(document).ready(function(){
+    $("#btn_signup").click(function(){
+
+        event.preventDefault();
+
+        var fname = $('#txt_fname').val().trim();
+        var lname = $('#txt_lname').val().trim();
+        var username = $('#txt_uname').val().trim();
+        var address = $('#txt_address').val().trim();
+        var email = $('#txt_email').val().trim();
+        var phone = $('#txt_phone').val().trim();
+        var password = $('#txt_psw').val().trim();
+        var rpt_password = $('#txt_psw-repeat').val().trim();
+
+        if( username != "" && password != "" && password === rpt_password){
+            var jsonObj = {firstName:fname,lastName:lname,phoneNumber: phone,
+                address: address, email : email, username : username, password : password};
+            $.ajax({
+                url:'http://localhost:8080/api/user/register',
+                type:'post',
+                contentType: "application/json; charset=utf-8",
+                data:JSON.stringify(jsonObj),
+                success:function(response){
+
+                }
+            });
+        }
+    });
+});
+
+
+
+
+function updateOwner(user) {
+
+    var url = 'http://localhost:8080/api/owner/';
+    var userObj;
+    $.ajax({
+        url: url,
+        type: 'get',
+        data: {username: user},
+        success: function (response) {
+            userObj = response[0];
+            $("#txt_fname").attr("placeholder", userObj.firstName);
+            // $("#getAllReview").attr("onclick", reviewFunction);
+            $("#txt_lname").attr("value", userObj.lastName);
+            $("#txt_phone").attr("value", userObj.phoneNumber);
+            $("#txt_address").attr("value", userObj.address);
+            $("#txt_email").attr("value", userObj.email);
+            $("#txt_uname").attr("value", userObj.username);
+        }
+    });}
